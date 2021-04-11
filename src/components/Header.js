0d4f61c9b68044360c16c6c168fbaf8d17162bd5
Alissonv2 <alissonvitorino8@gmail.com>
2021-04-11 16:40:29 -0300

    Added search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, StyleSheet, Image} from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
 import logo from '../../assets/image/marvelLogo.png';
 
 export default function Header() {
+  Icon.loadFont();
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
+      <Icon name="search" size={40} color="#000" />
     </View>
   );
 }
@@ -16,6 +19,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
+    flexDirection: 'row',
     width: '100%',
     height: 60,
     backgroundColor: '#de041e',
